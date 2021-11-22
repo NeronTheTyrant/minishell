1c23355eb6c982ea65a73191eb2c2cead7eefd44
Martin LEBARD <mlebard@e2r2p8.clusters.42paris.fr>
2021-11-22 18:42:06 +0100

    [dev] added more env helper functions, will organise them tomorrow in their own header then start working on builtins

--- srcs/testmain.c
+++ srcs/testmain.c
@@ -25,6 +25,67 @@ char	*ft_getenv(char	*var, char **env)
 	return (NULL);
 }
 
+int	ft_getenvi(char *var, char **env)
+{
+	int	i;
+	int	varlen;
+
+	if (!var || !env)
+		return (-1);
+	varlen = ft_strlen(var);
+	i = 0;
+	while (env[i])
+	{
+		if (ft_strncmp(var, env[i], varlen) == 0 && env[i][varlen] == '=')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+char	*make_envvar(char *var, char *val)
+{
+	int		varsize;
+	char	*envvar;
+
+	varsize = ft_strlen(var) + ft_strlen(val) + 2;
+	envvar = malloc(sizeof(*envvar) * (varsize));
+	if (envvar == NULL)
+		return (NULL);
+	ft_bzero(envvar, sizeof(*envvar) * varsize);
+	ft_strlcat(envvar, var, varsize);
+	ft_strlcat(envvar, "=", varsize);
+	ft_strlcat(envvar, val, varsize);
+	return (envvar);
+}
+
+int	ft_setenv(char *var, char *val, char ***env)
+{
+	int		envlen;
+	int		varindex;
+	char	*envvar;
+
+	envlen = ft_argcount(*env);
+	varindex = ft_getenvi(var, *env);
+	envvar = make_envvar(var, val);
+	if (envvar == NULL)
+		return (-1);
+	if (varindex == -1)
+	{
+		*env = ft_realloc(*env, sizeof(**env) * (envlen),
+			sizeof(**env) * (envlen + 1));
+		if (*env == NULL)
+			return (-1);
+		*env[envlen] = envvar;
+		*env[envlen + 1] = NULL;
+	}
+	else
+	{
+		free(*env[varindex]);
+		*env[varindex] = envvar;
+	}
+}
+
 char	**make_env(char **env)
 {
 	int		i;
@@ -36,6 +97,8 @@ char	**make_env(char **env)
 	{
 		new_env = ft_realloc(new_env, sizeof(*new_env) * (i + 1),
 			sizeof(*new_env) * (i + 2));
+		if (new_env == NULL)
+			return (NULL);
 		new_env[i] = ft_strdup(env[i]);
 		i++;
 	}
