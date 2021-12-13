c0ffc3c390ec0bb9ea5a8e74bdc4d95650d524d3
Aurelien CABIAC <acabiac@e3r12p18.clusters.42paris.fr>
2021-12-13 20:43:25 +0100

    done pwd and started cd

--- srcs/builtin/cd.c
+++ srcs/builtin/cd.c
@@ -1,20 +1,65 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cd_builtin.c                                       :+:      :+:    :+:   */
+/*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mlebard <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 15:10:16 by mlebard           #+#    #+#             */
-/*   Updated: 2021/12/09 15:10:29 by mlebard          ###   ########.fr       */
+/*   Updated: 2021/12/13 20:41:47 by acabiac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "core.h"
+#include "libft.h"
+#include "env.h"
+
+int	search_in_cdpath(char **curpath, t_term *t)
+{
+	(void)(curpath);
+	(void)(t);
+	return (0);
+}
+
+char	*init_curpath(char *arg, t_term *t)
+{
+	char	*curpath;
+
+	curpath = NULL;
+	if (arg[0] == '/')
+	{
+		curpath = ft_strdup(arg);
+	}
+	else if (ft_strncmp(arg, "./", 2) != 0 && ft_strncmp(arg, "../", 3) != 0)
+	{
+		if (search_in_cdpath(&curpath, t) != 1)
+			return (curpath);
+	}
+	return (curpath);
+}
 
 int	ft_cd(char **args, t_term *t)
 {
-	(void)args;
-	(void)t;
+	char	*curpath;
+	char	*arg;
+
+	arg = NULL;
+	if (args[1] == NULL)
+	{
+		arg = ft_getenv("HOME", t->env);
+		if (arg == NULL)
+		{
+			ft_putendl_fd("cd: HOME not set", 2);
+			return (1);
+		}
+	}
+	else if (args[2] != NULL)
+	{
+		ft_putendl_fd("cd: too many arguments", 2);
+		return (1);
+	}
+	else
+		arg = args[1];
+	curpath = init_curpath(arg, t);
 	return (0);
 }
--- srcs/builtin/pwd.c
+++ srcs/builtin/pwd.c
@@ -1,19 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pwd_builtin.c                                      :+:      :+:    :+:   */
+/*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mlebard <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 15:11:34 by mlebard           #+#    #+#             */
-/*   Updated: 2021/12/09 15:11:44 by mlebard          ###   ########.fr       */
+/*   Updated: 2021/12/13 16:59:59 by acabiac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "core.h"
+#include "error.h"
+#include <unistd.h>
 
 int	ft_pwd(char **args, t_term *t)
 {
+	char	*dir_name;
+
+	dir_name = getcwd(NULL, 0);
+	if (dir_name == NULL)
+		return (error_fatal(ERR_MALLOC, NULL));
+	ft_putendl_fd(dir_name, 1);
+	free(dir_name);
 	(void)args;
 	(void)t;
 	return (0);
